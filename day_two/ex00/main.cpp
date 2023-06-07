577e03bf56d5ce3cc5fe41bf8d2f06a833b4428e
Felipe Camargo <lufelip2@student.42sp.org.br>
2023-06-07 02:50:08 -0300

    sleep

---
+++ day_two/ex00/main.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lufelip2 <lufelip2@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/07 02:48:41 by lufelip2          #+#    #+#             */
+/*   Updated: 2023/06/07 02:49:28 by lufelip2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "Fixed.hpp"
+
+int main(void)
+{
+	Fixed	a;
+	Fixed	b( a );
+	Fixed	c;
+
+	c = b;
+
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+
+	return (0);
+}
